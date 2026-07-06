62213e7955bf350cefdabbcb832fabf04f80d7d1
thiagokoch <thiago.koch.pereira@escola.pr.gov.br>
2026-07-06 17:21:51 -0300

    Create ex4.js

---
+++ backend_thiago/aula5/ex4.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+const produtos = [
+    { id: 1, nome: "Mouse", preco: 80 },
+    { id: 2, nome: "Teclado", preco: 120 },
+    { id: 3, nome: "Monitor", preco: 900 },
+    { id: 4, nome: "Fone de Ouvido", preco: 150 }
+];
+
+app.get("/produtos/caros", (req, res) => {
+    const produtosCaros = produtos.filter(produto => produto.preco > 100);
+    res.json(produtosCaros);
+});
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000");
+});
