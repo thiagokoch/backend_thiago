3cd579f49929739b481bc5a2882ba9fde2a58c84
thiagokoch <thiago.koch.pereira@escola.pr.gov.br>
2026-07-06 17:09:06 -0300

    Update ex1.js

--- backend_thiago/lista-de-exercicios1/ex1.js
+++ backend_thiago/lista-de-exercicios1/ex1.js
@@ -1,1 +1,4 @@
-
+const input = require('readline-sync')
+const temp = input.question("qual o número da temperatura pra conversão?")
+let fahrenheit = temp * 1.8 + 32
+console.log(`${temp}°C corresponde a ${fahrenheit}°F`)
